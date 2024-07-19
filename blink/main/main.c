f88071db6f84b11f7117b8e4af76c8ffa2829317
Ryan <113010586+b11901040@users.noreply.github.com>
2024-07-19 12:25:17 +0800

    remove #define

--- blink/main/main.c
+++ blink/main/main.c
@@ -3,15 +3,14 @@
 #include "freertos/task.h"
 #include "driver/gpio.h"
 
-#define LED_GPIO_PIN GPIO_NUM_2
 
 void app_main(void) {
-    gpio_set_direction(LED_GPIO_PIN, GPIO_MODE_OUTPUT);
+    gpio_set_direction(GPIO_NUM_2, GPIO_MODE_OUTPUT);
 
     while (1) {
-        gpio_set_level(LED_GPIO_PIN, 1);
+        gpio_set_level(GPIO_NUM_2, 1);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
-        gpio_set_level(LED_GPIO_PIN, 0);
+        gpio_set_level(GPIO_NUM_2, 0);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
 }
