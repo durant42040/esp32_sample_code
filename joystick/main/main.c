9e41829ea28c264911df95b694f255c6cc65382f
Ryan <113010586+b11901040@users.noreply.github.com>
2024-07-19 16:01:52 +0800

    remove #define

--- joystick/main/main.c
+++ joystick/main/main.c
@@ -4,21 +4,18 @@
 #include "esp_system.h"
 #include "driver/adc.h"
 
-#define X_PIN ADC1_CHANNEL_4  // X 軸連接到 ADC1 的第 32 通道
-#define Y_PIN ADC1_CHANNEL_7  // Y 軸連接到 ADC1 的第 35 通道
-#define Z_PIN ADC1_CHANNEL_6  // Z 軸連接到 ADC1 的第 34 通道
 
 void setup_adc() {
     adc1_config_width(ADC_WIDTH_BIT_12);
-    adc1_config_channel_atten(X_PIN, ADC_ATTEN_DB_12);
-    adc1_config_channel_atten(Y_PIN, ADC_ATTEN_DB_12);
-    adc1_config_channel_atten(Z_PIN, ADC_ATTEN_DB_12);
+    adc1_config_channel_atten(ADC1_CHANNEL_4, ADC_ATTEN_DB_12);
+    adc1_config_channel_atten(ADC1_CHANNEL_7, ADC_ATTEN_DB_12);
+    adc1_config_channel_atten(ADC1_CHANNEL_6, ADC_ATTEN_DB_12);
 }
 
 void read_joystick_values() {
-    int x_val = adc1_get_raw(X_PIN);
-    int y_val = adc1_get_raw(Y_PIN);
-    int z_val = adc1_get_raw(Z_PIN);
+    int x_val = adc1_get_raw(ADC1_CHANNEL_4);
+    int y_val = adc1_get_raw(ADC1_CHANNEL_7);
+    int z_val = adc1_get_raw(ADC1_CHANNEL_6);
 
     printf("X axis value: %d\n", x_val);
     printf("Y axis value: %d\n", y_val);
